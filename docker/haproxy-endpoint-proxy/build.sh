747d11c0030eefbfbab8b72e02882625e42ae7b6
jmuraski <muraskijj@hotmail.com>
2015-01-12 20:38:36 -0600

    changd the haproxy endpoint to use debian-base and changed build to push to docker hub

--- docker/haproxy-endpoint-proxy/build.sh
+++ docker/haproxy-endpoint-proxy/build.sh
@@ -2,6 +2,6 @@
 
 docker build -t endpoint-haproxy .
 
-docker tag endpoint-haproxy quay.io/jmuraski/endpoint-haproxy
+docker tag endpoint-haproxy jmuraski/endpoint-haproxy
 
-docker push quay.io/jmuraski/endpoint-haproxy
+docker push jmuraski/endpoint-haproxy
